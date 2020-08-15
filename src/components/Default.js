cc7f7b470734640e6a35a70b0330f6b5f7f66941
Doris Li <shiyeli@Doriss-MacBook-Pro-2.local>
2020-08-15 16:57:54 -0400

    first commit for store project

---
+++ src/components/Default.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Default extends Component {
+    render() {
+        console.log(this.props);
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto text-center text-title text-uppercase pt-5">
+                        <h1 className="display-3">404</h1>
+                        <h3>the requested URL <span>{this.props.location.pathname}</span> {" "}was not found</h3>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
